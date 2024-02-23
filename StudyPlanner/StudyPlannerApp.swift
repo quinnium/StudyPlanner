e78a0816e453f96b10f835bd3f4973810d0e82f5
quinnium <78171763+quinnium@users.noreply.github.com>
2024-02-23 12:50:19 +0000

    Initial Commit

---
+++ StudyPlanner/StudyPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyPlannerApp.swift
+//  StudyPlanner
+//
+//  Created by Quinn on 23/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct StudyPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
